01fee59508a05949af65efbc4bdf70abe766b282
LaamGinghong <lanjinghang@banggood.com>
2018-11-28 17:46:34 +0800

    初始化脚手架

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import {ShareModule} from './share/share.module';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ShareModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
