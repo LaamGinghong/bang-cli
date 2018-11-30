98a9937f1879d0e90794b292587a01b6cbb299a6
LaamGinghong <lanjinghang@banggood.com>
2018-11-30 09:32:32 +0800

    修复拦截器cookie过时登录拦截

--- src/app/core/service/http-interceptor.service.ts
+++ src/app/core/service/http-interceptor.service.ts
@@ -2,7 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
 import {Observable, Observer, throwError} from 'rxjs';
 import {catchError, mergeMap} from 'rxjs/operators';
-import {NzNotificationService} from 'ng-zorro-antd';
+import {NzModalService, NzNotificationService} from 'ng-zorro-antd';
+import {CookieService} from '../../share/service/cookie.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,8 @@ import {NzNotificationService} from 'ng-zorro-antd';
 export class HttpInterceptorService implements HttpInterceptor {
 
   constructor(
-    private notification: NzNotificationService
+    private notification: NzNotificationService,
+    private modal: NzModalService
   ) {
   }
 
@@ -27,8 +29,29 @@ export class HttpInterceptorService implements HttpInterceptor {
           case 401:
             break;
           case 200:
-            if (res.body && !res.body.success) {
-              res.body.errorInfos[0].msg === '用户信息校验失败' ? this.notification.error('用户信息校验失败', '未能获取当前用户搜索条件设置，请确认账号登录成功后再次尝试！') : this.notification.error('失败', res.body.errorInfos[0].msg);
+            // if (res.body && !res.body.success) {
+            //   res.body.errorInfos[0].msg === '用户信息校验失败' ? this.notification.error('用户信息校验失败', '未能获取当前用户搜索条件设置，请确认账号登录成功后再次尝试！') : this.notification.error('失败', res.body.errorInfos[0].msg);
+            // }
+            if (res.body.errorInfos[0].msg === '请登录后再操作' || res.body.errorInfos[0].msg === '用户登录信息过期') {
+              this.modal.create({
+                nzWidth: 340,
+                nzMaskClosable: false,
+                nzTitle: `登录提示`,
+                nzContent: `登录超时，请重新登录`,
+                nzFooter: [
+                  {
+                    label: '去登录',
+                    shape: 'primary',
+                    onClick: () => {
+                      this.modal.closeAll();
+                      CookieService.delete('WSP_JSESSIONID');
+                      window.location.reload();
+                    }
+                  }
+                ]
+              });
+            } else {
+              this.notification.create('error', '失败', res.body.errorInfos[0].msg);
             }
             break;
           case 404:
