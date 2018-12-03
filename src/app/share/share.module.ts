85fa147772aeb879f83fa380db3906611c923a59
LaamGinghong <lanjinghang@banggood.com>
2018-12-03 17:25:04 +0800

    修改message、notification全局配置

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,10 +1,19 @@
 import {NgModule} from '@angular/core';
-import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import {NgZorroAntdModule, NZ_I18N, NZ_MESSAGE_CONFIG, NZ_NOTIFICATION_CONFIG, NzMessageConfig, NzNotificationConfig, zh_CN} from 'ng-zorro-antd';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {ApiService} from './service/api.service';
 import {CookieService} from './service/cookie.service';
 import {CommonMethodsService} from './service/common-methods.service';
+
+const notificationConfig: NzNotificationConfig = {
+  nzDuration: 2000,
+  nzMaxStack: 3
+};
+const messageConfig: NzMessageConfig = {
+  nzDuration: 3000,
+  nzMaxStack: 1
+};
 
 @NgModule({
   declarations: [],
@@ -22,6 +31,8 @@ import {CommonMethodsService} from './service/common-methods.service';
   ],
   providers: [
     {provide: NZ_I18N, useValue: zh_CN},
+    {provide: NZ_NOTIFICATION_CONFIG, useValue: notificationConfig},
+    {provide: NZ_MESSAGE_CONFIG, useValue: messageConfig},
     ApiService,
     CookieService,
     CommonMethodsService
